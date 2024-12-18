c4481700a8143e9dcfb12a8ef56a360955028dc4
sajjad <sajjadsajjad098765@gmail.com>
2024-12-18 23:56:51 +0600

    global error handling

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,4 +6,5 @@ export default {
   port: process.env.PORT,
   mongodbUrl: process.env.MONGODB_URL,
   bcryptSaltRounds: process.env.BCRYPT_SALT_ROUNDS,
+  nodeEnv: process.env.NODE_ENV,
 };
---
+++ src/middleware/globalErrorHandler.ts
@@ -0,0 +1,68 @@
+import { ZodError } from "zod";
+import { TErrorSource } from "../interface/globalInterface";
+import handleZodError from "../error/handelZodError";
+import { ErrorRequestHandler } from "express";
+import handleValidationError from "../error/handleValidationError";
+import handleCastError from "../error/handleCastError";
+import AppError from "../error/AppError";
+import config from "../config";
+
+const globalErrorHandler: ErrorRequestHandler = (error, req, res) => {
+  let statusCode = 500;
+  let message = "Something went wrong";
+
+  let errorSource: TErrorSource = [
+    {
+      path: "",
+      message: "Something went wrong",
+    },
+  ];
+
+  // checking there it's zod error or validation error
+
+  if (error instanceof ZodError) {
+    const simplifiedError = handleZodError(error);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSource = simplifiedError?.errorSource;
+  } else if (error?.name === "ValidationError") {
+    const simplifiedError = handleValidationError(error);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSource = simplifiedError?.errorSource;
+  } else if (error?.name === "CastError") {
+    const simplifiedError = handleCastError(error);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSource = simplifiedError?.errorSource;
+  } else if (error instanceof AppError) {
+    statusCode = error?.statusCode;
+    message = error?.message;
+    errorSource = [
+      {
+        path: "",
+        message: error?.message,
+      },
+    ];
+  } else if (error instanceof Error) {
+    message = error?.message;
+    errorSource = [
+      {
+        path: "",
+        message: error?.message,
+      },
+    ];
+  }
+
+  // sending response
+
+  res.status(statusCode).json({
+    success: false,
+    message,
+    errorSource,
+    error,
+    stack: config.nodeEnv === "development" ? error?.stack : null,
+  });
+};
+
+export default globalErrorHandler;
