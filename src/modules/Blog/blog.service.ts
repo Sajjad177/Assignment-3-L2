5b07c920f2f1012ace738a3cb9afb7f79c4ce5e9
sajjad <sajjadsajjad098765@gmail.com>
2024-12-21 19:42:50 +0600

    assignment clear

--- src/modules/Blog/blog.service.ts
+++ src/modules/Blog/blog.service.ts
@@ -5,10 +5,10 @@ import { Blog } from "./blog.model";
 
 const createBlogInDB = async (payload: TBlog) => {
   const { author, ...blogData } = payload;
-  const isAuthorExist = await User.findById(author);
 
+  const isAuthorExist = await User.findById(author);
   if (!isAuthorExist) {
-    throw new Error("Author not found");
+    throw new Error("Author is not found");
   }
 
   const result = await Blog.create({ ...blogData, author });
